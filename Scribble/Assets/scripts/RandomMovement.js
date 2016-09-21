ce332c7cd9c6f17e55a710885d96911e5f9b143f
Stephanie Mendoza <stephanie.a.mendoza.00@gmail.com>
2016-09-20 17:46:06 -0700

    Scribble-VR Unity Build
    Scribble World Version 1.0
    
    s c r i b b l e s

---
+++ Scribble/Assets/scripts/RandomMovement.js
@@ -0,0 +1,23 @@
+﻿ var target:Vector3;
+ var timer:float;
+ var sec:int;
+ function Start () {
+  target = ResetTarget();
+  sec=ResetSec();
+ }
+ 
+ function Update () {
+ timer+=Time.deltaTime;
+ if(timer>sec){
+ target=ResetTarget();
+ sec=ResetSec();
+ }
+ transform.Translate(target*10*Time.deltaTime);
+ }
+ function ResetTarget():Vector3{
+     return Vector3(Random.Range(-2.0,2.0),Random.Range(-2.0,2.0),Random.Range(-2.0,2.0));
+ }
+ function ResetSec():int{
+     timer =0;
+     return Random.Range(1,3);
+ }
---
+++ Scribble/Assets/scripts/sceneChange.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class sceneChange : MonoBehaviour {
+
+	public string levelToLoad;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Player") 
+		{
+			SceneManager.LoadScene(levelToLoad);
+		}	
+	}
+}
